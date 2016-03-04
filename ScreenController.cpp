75518168fbd7d781352af9fc5b7b2b8af72cffaa
Ricard Perez del Campo <ricardperez@Ricards-iMac.local>
2016-03-04 09:13:23 +0100

    Added the Screen classes - taken from the KillyCraft project.

---
+++ ScreenController.cpp
@@ -0,0 +1,46 @@
+//
+//  ScreenController.cpp
+//  KillyCraft
+//
+//  Created by Ricard Perez del Campo on 23/12/14.
+//  Copyright (c) 2014 MelonGames. All rights reserved.
+//
+
+#include "ScreenController.h"
+#include "Screen.h"
+#include "base/CCDirector.h"
+#include "2d/CCTransition.h"
+
+namespace MelonGames
+{
+    ScreenController::ScreenController()
+    : currentScreen(nullptr)
+    {
+    }
+    
+    ScreenController* ScreenController::getInstance()
+    {
+        static ScreenController singleton;
+        return &singleton;
+    }
+    
+    void ScreenController::replaceScreen(Screen* screen)
+    {
+        auto prevScreen = currentScreen;
+        currentScreen = screen;
+        
+        auto scene = cocos2d::Scene::create();
+        scene->addChild(screen);
+        
+        auto director = cocos2d::Director::getInstance();
+        if (prevScreen == nullptr)
+        {
+            director->runWithScene(scene);
+        }
+        else
+        {
+            auto transitionScene = cocos2d::TransitionMoveInR::create(0.5f, scene);
+            director->replaceScene(transitionScene);
+        }
+    }
+}
